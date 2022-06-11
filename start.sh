69b25c3e077c3cd08e75e7473d5be64c4189df61
A H A D <102145663+ahad0xff@users.noreply.github.com>
2022-06-11 13:45:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,16 @@
-python3 update.py && python3 -m bot
+UPSTREAM_REPO="https://github.com/ahad0xff/admlb"
+UPSTREAM_BRANCH="main"
+MAINDIR=/usr/src/app
+
+mkdir $MAINDIR && chmod 777 $MAINDIR && cd $MAINDIR
+
+git init -q
+git config --global user.email ahadmirrorlab1@gmail.com
+git config --global user.name ahadz-git
+git add .
+git commit -sm update -q
+git remote add origin $UPSTREAM_REPO
+git fetch origin -q
+git reset --hard origin/$UPSTREAM_BRANCH -q
+
+python3 -m bot
