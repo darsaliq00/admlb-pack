8eca0217c49eb1a0b7d4a9452c91cab1e99a41b3
Ahad Shaikh <102145663+ahad0xff@users.noreply.github.com>
2022-08-03 01:06:37 +0530

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -7,7 +7,7 @@ libssl-dev libc-ares-dev libsodium-dev libcrypto++-dev \
 libsqlite3-dev libfreeimage-dev libpq-dev libffi-dev \
 && locale-gen en_US.UTF-8
 
-# MegaSDK
+# MegaSdkRest
 curl -L https://github.com/anasty17/megasdkrest/releases/download/latest/megasdkrest-$(cpu=$(uname -m);\
 if [[ "$cpu" == "x86_64" ]]; then echo "amd64"; elif [[ "$cpu" == "x86" ]]; \
 then echo "i386"; elif [[ "$cpu" == "aarch64" ]]; then echo "arm64"; else echo $cpu; fi) \
