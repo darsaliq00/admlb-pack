481bcb46de41a829be0211b25ac7ff46d9d15a6b
Ahad Shaikh <102145663+ahad0xff@users.noreply.github.com>
2022-08-03 00:57:45 +0530

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
-# Packages and MegaSDK
+# Packages
 apt-get -y update && DEBIAN_FRONTEND="noninteractive" \
 apt-get install -y python3 python3-pip aria2 qbittorrent-nox \
 mediainfo tzdata p7zip-full p7zip-rar xz-utils curl pv jq ffmpeg \
@@ -7,6 +7,7 @@ libssl-dev libc-ares-dev libsodium-dev libcrypto++-dev \
 libsqlite3-dev libfreeimage-dev libpq-dev libffi-dev \
 && locale-gen en_US.UTF-8
 
+# MegaSDK
 curl -L https://github.com/anasty17/megasdkrest/releases/download/latest/megasdkrest-$(cpu=$(uname -m);\
 if [[ "$cpu" == "x86_64" ]]; then echo "amd64"; elif [[ "$cpu" == "x86" ]]; \
 then echo "i386"; elif [[ "$cpu" == "aarch64" ]]; then echo "arm64"; else echo $cpu; fi) \
